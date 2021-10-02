66a512b1ae1e7d1d5bdf473b65c7a6d1721432bd
MikeGer <mikeman@inbox.ru>
2021-10-02 15:56:55 +0300

    added product-cards and add_button

--- src/js/script.js
+++ src/js/script.js
@@ -49,12 +49,159 @@ onload = function () {
         price: 80,
         img: '/src/catalog/images/pr7.jpg'
     },
+//     {
+//       uin_product: 'pr8',
+//       group: 'milkProd',
+//       name: 'Молоко',
+//       price: 60,
+//       img: "src/catalog/images/pr1.jpg"
+//   },
+//   {
+//     uin_product: 'pr9',
+//     group: 'milkProd',
+//     name: 'Молоко',
+//     price: 60,
+//     img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr10',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr11',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr12',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr13',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr14',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr15',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr16',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr17',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr18',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr19',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr20',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr21',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr22',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr23',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr24',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr25',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr26',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr27',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
+// {
+//   uin_product: 'pr28',
+//   group: 'milkProd',
+//   name: 'Молоко',
+//   price: 60,
+//   img: "src/catalog/images/pr1.jpg"
+// },
 
 ];
   
   const renderGoodsItem = (img, name, price) => {
     
-    return `<div class="goods-item"><img class="product_photo" src=${img}></div><h3>${name}</h3><p>${price}</p></div>`;
+    return `<div class="goods-item"><img class="product_photo" src=${img}><h3>${name}</h3><p>${price}</p><button class="add_btn">добавить</button></div>`;
   };
   
   const renderGoodsList = (list) => {
