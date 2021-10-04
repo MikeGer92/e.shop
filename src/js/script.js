1555499b4f72d594bdf0a34eb840a8dddfea4636
MikeGer <mikeman@inbox.ru>
2021-10-04 11:23:46 +0300

    changed style and prices

--- src/js/script.js
+++ src/js/script.js
@@ -4,49 +4,49 @@ onload = function () {
         uin_product: 'pr1',
         group: 'milkProd',
         name: 'Молоко',
-        price: 60,
+        price: 60.00,
         img: "src/catalog/images/pr1.jpg"
     },
     {
         uin_product: 'pr2',
         group: 'milkProd',
         name: 'Йогурт',
-        price: 32,
+        price: 32.00,
         img: 'src/catalog/images/pr2.jpg'
     },
     {
         uin_product: 'pr3',
         group: 'breadProd',
         name: 'Багет',
-        price: 40,
+        price: 40.00,
         img: 'src/catalog/images/pr3.jpg'
     },
     {
         uin_product: 'pr4',
         group: 'breadProd',
         name: 'Круасан',
-        price: 220,
+        price: 220.00,
         img: 'src/catalog/images/pr4.jpg'
     },
     {
         uin_product: 'pr5',
         group: 'cheeseProd',
         name: 'Сыр',
-        price: 640,
+        price: 640.00,
         img: 'src/catalog/images/pr5.jpg'
     },
     {
         uin_product: 'pr6',
         group: 'vegProd',
         name: 'Перец',
-        price: 270,
+        price: 270.00,
         img: '/src/catalog/images/pr6.jpg'
     },
     {
         uin_product: 'pr7',
         group: 'fruProd',
         name: 'Яблоки',
-        price: 80,
+        price: 80.00,
         img: '/src/catalog/images/pr7.jpg'
     },
 //     {
@@ -151,7 +151,7 @@ onload = function () {
 //   uin_product: 'pr22',
 //   group: 'milkProd',
 //   name: 'Молоко',
-//   price: 60,
+//   price: 60.00,
 //   img: "src/catalog/images/pr1.jpg"
 // },
 // {
